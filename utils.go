bfa1941acbff1eb02095d7f707dc38b630e458f4
monirzadeh <25131576+Monirzadeh@users.noreply.github.com>
2023-12-28 18:58:22 +0330

    simpler s2b conversion

--- utils.go
+++ utils.go
@@ -4,7 +4,6 @@ import (
 	"encoding/base64"
 	"fmt"
 	nurl "net/url"
-	"reflect"
 	"regexp"
 	"strings"
 	"unsafe"
@@ -93,12 +92,7 @@ func createDataURL(content []byte, contentType string) string {
 
 // s2b converts string to a byte slice without memory allocation.
 func s2b(s string) (b []byte) {
-	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	bh.Data = sh.Data
-	bh.Cap = sh.Len
-	bh.Len = sh.Len
-	return b
+	return []byte(s)
 }
 
 // b2s converts byte slice to a string without memory allocation.
